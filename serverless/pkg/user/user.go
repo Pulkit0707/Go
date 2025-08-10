e2a5a09b876c774c86ac98b2f7a298868f6237b3
Pulkit0707 <pulkit0707chawla@gmail.com>
2025-08-10 23:47:13 +0530

    add all error methods and createUser function

--- serverless/pkg/user/user.go
+++ serverless/pkg/user/user.go
@@ -1,18 +1,28 @@
 package user
 
 import (
-	"errors"
-	"encoding/json"
 	"context"
+	"encoding/json"
+	"errors"
+	"serverless/pkg/validators"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 )
 
 var(
 	ErrorFailedToFetchRecord = "Failed to fetch Record"
 	ErrorFailedToUnmarshalRecord = "Failed to unmarshal record"
+	ErrorInvalidUserData = "Inavalid user data"
+	ErrorInvalidEmail = "Invalid email"
+	ErrorCouldNotMarshalItem = "Could not marshal item"
+	ErrorCouldNotDeleteItem = "Could not delete item"
+	ErrorCouldNotDynamoPutItem = "Could not dynamo put item"
+	ErrorUserAlreadyExists = "user.User already exists"
+	ErrorUserDoesNotExist = "user does not exist"
 )
 
 type User struct{
@@ -70,7 +80,32 @@ func FetchUsers(ctx context.Context, tableName string, dynaClient *dynamodb.Clie
 }
 
 
-func CreateUser(ctx context.Context,)(){}
+func CreateUser(ctx context.Context,req events.APIGatewayProxyRequest, tableName string, dynaClient *dynamodb.Client)(*User, error){
+	var u User
+	if err:= json.Unmarshal([]byte(req.Body),&u); err!=nil{
+		return nil, errors.New(ErrorInvalidUserData)
+	}
+	if !validators.IsEmailValid(u.Email){
+		return nil, errors.New(ErrorInvalidEmail)
+	}
+	currentUser,_:=FetchUser(ctx,u.Email,tableName,dynaClient)
+	if currentUser!=nil && len(currentUser.Email)!=0{
+		return nil, errors.New(ErrorUserAlreadyExists)
+	}
+	av,err := attributevalue.MarshalMap(u)
+	if err!=nil{
+		return nil, errors.New(ErrorCouldNotMarshalItem)
+	}
+	input:= &dynamodb.PutItemInput{
+		Item: av,
+		TableName: aws.String(tableName),
+	}
+	_,err= dynaClient.PutItem(ctx, input)
+	if err!=nil{
+		return nil, errors.New(ErrorCouldNotDynamoPutItem)
+	}
+	return &u, nil;
+}
 
 func UpdateUser(ctx context.Context,)(){}
 
