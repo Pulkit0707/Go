c2197762e64ba67b0b8c69b5bf4d6dab61ab9e39
Pulkit0707 <pulkit0707chawla@gmail.com>
2025-08-16 13:13:34 +0530

    complete reciever/consumer

--- rabbitmq/receive/receive.go
+++ rabbitmq/receive/receive.go
@@ -6,5 +6,43 @@ import(
 )
 
 func main(){
-	
+	conn,err := amqp.Dial("amqp://username:password@localhost:5672/")
+	failOnError(err, "failed to connect to RabbitMq")
+	defer conn.Close()
+	ch,err:=conn.Channel()
+	failOnError(err, "failed to open a channel")
+	defer ch.Close()
+	q,err:=ch.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "failed to decalre a queue")
+	msgs,err:=ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "failed to register a consumer")
+	var forever chan struct{}
+	go func(){
+		for d:=range msgs{
+			log.Printf("Received a message:%s", d.Body)
+		}
+	}()
+	log.Printf("[*] Wating for messages")
+	<-forever
+}
+
+func failOnError(err error, msg string){
+	if err!=nil{
+		log.Fatalf("%s:%s", err, msg)
+	}
 }
